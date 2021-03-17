48c0413b9383e0b74aa25226cff36f080672ede9
Oreo26 <75868375+Oreo26@users.noreply.github.com>
2021-03-16 23:29:31 -0700

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,6 @@
-console.log("OK");
+console.log('OK');
 
 // https://unpkg.co/gsap@3/dist/gsap.min.js
 
 /* Rotation of Red*/
-gsap.to(".Orangeicon", { rotation: 360, x: 330, duration: 2, ease: "none" });
-
-/* Rotation of Orange*/
-/*gsap.to(".Redicon", {rotation: 360,y: 200, duration: 2, ease: "none"});*/
+gsap.to(".Redicon", {rotation: 360,x: 1000, duration: 6, ease: "none"});

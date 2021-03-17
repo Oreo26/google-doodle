ca60441a8f3a7f0e29b26bf4aafc122d80c22cc7
Oreo26 <75868375+Oreo26@users.noreply.github.com>
2021-03-16 23:53:06 -0700

    Duration Changing the duration to be a bit faster

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-console.log('OK');
+console.log("OK");
 
 // https://unpkg.co/gsap@3/dist/gsap.min.js
 
 /* Rotation of Red*/
-gsap.to(".Redicon", {rotation: 360,x: 1000, duration: 6, ease: "none"});
+gsap.to(".Redicon", { rotation: 360, x: 1000, duration: 5, ease: "none" });
